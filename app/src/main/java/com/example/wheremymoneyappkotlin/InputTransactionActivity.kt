532155f53883be852aca91b342dcd42898e71ad8
RoeRL <bimo.17168@gmail.com>
2023-12-03 09:57:09 +0700

    [RR] Input Transaction Activity done
    Without firebase

--- app/src/main/java/com/example/wheremymoneyappkotlin/InputTransactionActivity.kt
+++ app/src/main/java/com/example/wheremymoneyappkotlin/InputTransactionActivity.kt
@@ -1,20 +1,34 @@
 package com.example.wheremymoneyappkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 
 class InputTransactionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input_transaction)
 
+        val m_itemForm = findViewById<EditText>(R.id.itemInput).text.toString()
+        val m_priceForm = findViewById<EditText>(R.id.priceInput).text.toString().toDoubleOrNull()
+        val m_itemLayout = findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.itemLayout)
+        val m_priceLayout = findViewById<com.google.android.material.textfield.TextInputLayout>(R.id.priceLayout)
         val m_inputIncomeButton = findViewById<Button>(R.id.inputIncomeButton)
+        val m_closeIncomeButton = findViewById<ImageButton>(R.id.closeIncomeTransaction)
+
+        m_closeIncomeButton.setOnClickListener {
+            finish()
+        }
 
         m_inputIncomeButton.setOnClickListener {
-            val m_itemForm = findViewById<EditText>(R.id.itemInput).text.toString()
-            val m_priceForm = findViewById<EditText>(R.id.priceInput).text.toString().toDoubleOrNull()
+
+
+            if (m_itemForm.isEmpty()) m_itemLayout.error = "Please input your item"
+            if (m_priceForm==null) m_priceLayout.error = "Please input your item"
         }
     }
 }
