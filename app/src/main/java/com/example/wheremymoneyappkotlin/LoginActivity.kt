7ebaec9dc219b993b600f863f863d13d31973255
RoeRL <bimo.17168@gmail.com>
2023-12-03 14:41:24 +0700

    [RR] Register Implement to Firebase

--- app/src/main/java/com/example/wheremymoneyappkotlin/LoginActivity.kt
+++ app/src/main/java/com/example/wheremymoneyappkotlin/LoginActivity.kt
@@ -1,36 +1,37 @@
 package com.example.wheremymoneyappkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-
     private lateinit var emailLogin: EditText
     private lateinit var passLogin: EditText
     private lateinit var loginButton: Button
-
-    private lateinit var mAuth: FirebaseAuth
+    private lateinit var registerButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_design)
-
-        mAuth = FirebaseAuth.getInstance()
+        setContentView(R.layout.activity_login)
 
         emailLogin = findViewById(R.id.emailForm)
         passLogin = findViewById(R.id.passwordForm)
         loginButton = findViewById(R.id.loginButton)
+        registerButton = findViewById(R.id.registerButton)
 
         //TODO: Get Database Login Information[email, password]
         //TODO: Check If Login Input is equal with the login information from database
 
         loginButton.setOnClickListener {
             userLoginFunction()
+        }
+
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 
@@ -44,22 +45,6 @@ class LoginActivity : AppCompatActivity() {
             Toast.makeText(this@LoginActivity, "Kolom Login atau Password Kosong!", Toast.LENGTH_SHORT).show()
             return
         }
-
-        mAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    // Login berhasil
-                    Toast.makeText(this@LoginActivity, "Login berhasil", Toast.LENGTH_SHORT).show()
-                    setContentView(R.layout.activity_main)
-                } else {
-                    // Login gagal
-                    Toast.makeText(
-                        this@LoginActivity,
-                        "Login gagal. Periksa email dan password Anda.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
 
         //TODO: Sign in with registered acc
 
